ece55019c206e554068cdfdaa0091444374c8ac6
JackLin-Yao <jacklinyao0702@163.com>
2022-05-04 08:40:22 +0800

    登录功能

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    Home组件
+  </div>
+</template>
+
+<script>
+export default {
+  setup () {
+    return {}
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 import './plugins/element.js'
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+// 导入全局样式表
+import './assets/css/global.css'
+// eslint-disable-next-line no-unused-vars
+import axios from 'axios'
+axios.defaults.baseURL = 'http://139.9.202.95:8888/api/private/v1'
+Vue.prototype.$http = axios
 
-createApp(App).use(store).use(router).mount('#app')
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
